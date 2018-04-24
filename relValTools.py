26dbd04cb93abbe457ea4aca7841b46b43e40daa
Olena Hlushchenko <greyxray@gmail.com>
2018-04-24 19:08:20 +0200

    move the argparser to common tools

--- relValTools.py
+++ relValTools.py
@@ -1,6 +1,26 @@
 import eostools
 import re
 
+def addArguments(parser):
+    # for produceTauValTree.py
+    parser.add_argument('runtype', choices=['ZTT', 'ZEE', 'ZMM', 'QCD', 'TTbar', 'TTbarTau', 'ZpTT'], help='choose sample type')
+    parser.add_argument('-r', '--release', default='CMSSW_9_4_0_pre2',  help='Release string [Default: %(default)s]')
+    parser.add_argument('-g', '--globalTag', default='PU25ns_94X_mc2017_realistic_v1-v1',  help='Global tag [Default: %(default)s]')
+    parser.add_argument('-n', '--maxEvents', default=-1, type=int,  help='Number of events that will be analyzed (-1 = all events) [Default: %(default)s]')
+    parser.add_argument('-u', '--useRecoJets', default=False, action="store_true",  help='Use RecoJets [Default: %(default)s]')
+    parser.add_argument('-s', '--storageSite', default='eos', choices=['eos','das', 'loc'], help="Choose between samples store on eos or DAS or in private local folder [Default: %(default)s]")
+    parser.add_argument('-l', '--localdir', default='/eos/user/o/ohlushch/relValMVA/', help="Local dir where the samples are looked up [Default: %(default)s]")
+    parser.add_argument('-i', '--inputfile', default='', help="Single file location for fast checks [Default: %(default)s]")
+    parser.add_argument('-d', '--debug', default=False, help="Debug option [Default: %(default)s]", action="store_true")
+    parser.add_argument('-m', '--mvaid', default=["2017v1", "2017v2", "newDM2017v2", "dR0p32017v2", "2016v1", "newDM2016v1"], nargs='*', help="Select final state(s) for measurement. This agument can be set multiple times. [Default: %(default)s]")
+    parser.add_argument('-t', '--tauCollection', default='slimmedTaus', help="Tau collection to be used. [Default: %(default)s]")
+    # for compare.py
+    parser.add_argument('-p', '--part',  help='Make WP plots(1), first half of histogram plots(2), \
+        second half of histogram plots(3), or everything at once(0) \
+        (This part needs to be split up to avoid a crash that happens for some reason)', default=0)
+    parser.add_argument('-b', '--onebin', action="store_true",  help='Plot inclusive efficiencies by only using one bin', default=False)
+
+
 def getFilesFromEOS(path, cmseospath=True):
     '''Give path in form /store/relval/CMSSW_9_4_0_pre2/...'''
     if path[-1] == "/": path = path[:-1]
@@ -15,6 +35,7 @@ def getFilesFromEOS(path, cmseospath=True):
 
     print "files:", files
     return files
+
 
 def getFilesFromDAS(release, runtype, globalTag):
     '''Get proxy with "voms-proxy-init -voms cms" to use this option.'''
@@ -37,6 +58,7 @@ runtype_to_sample = {
     'TTbarTau':'RelValTTbar_13',
     'ZpTT':'RelValZpTT_1500_13'
 }
+
 
 # Input source
 #key = '2017MCv2_W3Jets'
