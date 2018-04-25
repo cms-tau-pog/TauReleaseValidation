90eeb68e7634a0537e2331e18c90ba3892ca044a
Olena Hlushchenko <greyxray@gmail.com>
2018-04-25 14:48:06 +0200

    style corrections, visibility fixes

--- compare.py
+++ compare.py
@@ -10,9 +10,11 @@ from ROOT import gROOT, gStyle, TH1F, TFile, TCanvas, TPad, TLegend, TGraphAsymm
 from officialStyle import officialStyle
 from array import array
 import os
+import errno
+from collections import namedtuple
 import argparse
 from variables import vardict, hvardict
-
+from relValTools import *
 
 gROOT.SetBatch(True)
 officialStyle(gStyle)
@@ -21,21 +23,22 @@ gStyle.SetOptTitle(0)
 # set_palette("color")
 # gStyle.SetPaintTextFormat("2.0f")
 
-
-def ensureDir(directory):
+def ensureDir(file_path):
+    directory = os.path.dirname(file_path)
     if not os.path.exists(directory):
-        os.makedirs(directory)
+        try:
+            os.makedirs(directory)
+        except OSError as exc:
+            if exc.errno == errno.EEXIST and os.path.isdir(directory):
+                pass
+            else:
+                print "Bad name for directory:", file_path
+                raise
 
-
-def save(canvas, name):
-    ensureDir(name[0:name.find("/")])
-    ensureDir(name[0:name.rfind("/")])
-    #ensureDir('compare_' + runtype)
-
-    #canvas.SaveAs(name.replace(' ', '').replace('&&', '')+'.pdf')
-    #canvas.SaveAs(name.replace(' ', '').replace('&&', '')+'.gif')
-    canvas.SaveAs(name.replace(' ', '').replace('&&', '')+'.png')
-
+def save(canvas, name, extension='.png'):
+    name = name.replace(' ', '').replace('&&', '')
+    ensureDir(name)
+    canvas.SaveAs(name + extension)
 
 def configureLegend(leg, ncolumn):
     leg.SetNColumns(ncolumn)
@@ -46,14 +49,13 @@ def configureLegend(leg, ncolumn):
     leg.SetTextSize(0.02)
     leg.SetTextFont(42)
 
-
-def overlay(hists, ytitle, header, addon):
+def overlay(hists, ytitle, header, addon, runtype, tlabel, xlabel, dryrun=False):
+    print "overlay::len(hists)", len(hists)
+    _eta = "_eta" * ("_eta" in header)
     #print 'number of histograms = ', len(hists)
-
     canvas = TCanvas()
     leg = TLegend(0.2, 0.7, 0.5, 0.9)
     configureLegend(leg, 1)
-
     ymax = -1
     ymin = 100
 
@@ -66,48 +68,36 @@ def overlay(hists, ytitle, header, addon):
             x = Double(-1)
             y = Double(-1)
             hist.GetPoint(ip, x, y)
-
-            if ymin > y:
-                ymin = y
-            if ymax < y:
-                ymax = y
+            if ymin > y: ymin = y
+            if ymax < y: ymax = y
 
     for ii, hist in enumerate(hists):
         hist.SetMaximum(ymax*1.4)
         hist.SetMinimum(ymin*0.80)
-	#hist.GetXaxis().SetLimits(hist.GetXaxis().GetXmin()+(3*(ii-3)), hist.GetXaxis().GetXmax()+(3*(ii-3)))
-
-    #for ii, hist in enumerate(hists):
-        if ii == 0:
-            hist.Draw("Ap")
-        else:
-            hist.Draw("psame")
-
+        #hist.GetXaxis().SetLimits(hist.GetXaxis().GetXmin()+(3*(ii-3)), hist.GetXaxis().GetXmax()+(3*(ii-3)))
+        if ii == 0: hist.Draw("Ap")
+        else:       hist.Draw("psame")
         legname = hist.GetName()
-	hist.GetPoint(0, x, y)
-
+        hist.GetPoint(0, x, y)
         leg.AddEntry(hist, legname, 'lep')
-
-
-
     leg.Draw()
 
-    tex = TLatex(hists[-1].GetXaxis().GetXmin() + 0.01*(hists[-1].GetXaxis().GetXmax() -
-                                                        hists[-1].GetXaxis().GetXmin()), ymax*1.4, addon.replace('tau_', ''))
-
+    tex = TLatex(hists[-1].GetXaxis().GetXmin()
+        + 0.01*(hists[-1].GetXaxis().GetXmax()
+        - hists[-1].GetXaxis().GetXmin()),
+        ymax*1.4, addon.replace('tau_', ''))
     tex.SetTextAlign(10)
     tex.SetTextFont(42)
     tex.SetTextSize(0.03)
     tex.Draw()
 
     xshift = 0.87
-    # xshift=0.7
-    if tlabel.find('QCD') != -1:
-        xshift = 0.6
-    if runtype.find('TTbarTau') != -1:
-        xshift = 0.78
-    tex2 = TLatex(hists[-1].GetXaxis().GetXmin() + xshift*(hists[-1].GetXaxis(
-    ).GetXmax() - hists[-1].GetXaxis().GetXmin()), ymax*1.4, tlabel)
+    if tlabel.find('QCD') != -1:       xshift = 0.6
+    if runtype.find('TTbarTau') != -1: xshift = 0.78
+    tex2 = TLatex(hists[-1].GetXaxis().GetXmin()
+            + xshift*(hists[-1].GetXaxis().GetXmax()
+            - hists[-1].GetXaxis().GetXmin()),
+            ymax*1.4, tlabel)
 
     tex2.SetTextAlign(10)
     tex2.SetTextFont(42)
@@ -115,33 +105,59 @@ def overlay(hists, ytitle, header, addon):
     tex2.Draw()
 
     directory1 = ""
-    directory1options = ["byLooseCombinedIsolationDeltaBetaCorr3Hits", "byMediumCombinedIsolationDeltaBetaCorr3Hits", "byTightCombinedIsolationDeltaBetaCorr3Hits", "byPtOutOfCone", "byLooseChargedIsolation", "byMediumChargedIsolation", "byTightChargedIsolation", "byLooseNeutralIsolation", "byMediumNeutralIsolation", "byTightNeutralIsolation", "byLooseNeutralIsolationUnCorr", "byMediumNeutralIsolationUnCorr", "byTightNeutralIsolationUnCorr", "byLooseIsolationMVArun2v1DBoldDMwLT", "byMediumIsolationMVArun2v1DBoldDMwLT", "byTightIsolationMVArun2v1DBoldDMwLT", "againstElectron", "againstMuon", "DecayModeFinding"]
+    directory1options = ["byLooseCombinedIsolationDeltaBetaCorr3Hits",
+        "byMediumCombinedIsolationDeltaBetaCorr3Hits",
+        "byTightCombinedIsolationDeltaBetaCorr3Hits",
+        "byPtOutOfCone",
+        "byLooseChargedIsolation",
+        "byMediumChargedIsolation",
+        "byTightChargedIsolation",
+        "byLooseNeutralIsolation",
+        "byMediumNeutralIsolation",
+        "byTightNeutralIsolation",
+        "byLooseNeutralIsolationUnCorr",
+        "byMediumNeutralIsolationUnCorr",
+        "byTightNeutralIsolationUnCorr",
+        "byLooseIsolationMVArun2v1DBoldDMwLT",
+        "byMediumIsolationMVArun2v1DBoldDMwLT",
+        "byTightIsolationMVArun2v1DBoldDMwLT",
+        "againstElectron",
+        "againstMuon",
+        "DecayModeFinding",
+        'byLooseIsolationMVArun2017v2DBoldDMwLT2017',
+        'byMediumIsolationMVArun2017v2DBoldDMwLT2017',
+        'byTightIsolationMVArun2017v2DBoldDMwLT2017']
     directory2 = ""
-    directory2options = {"_1p":"1prong", "_2p":"2prong", "_3p":"3prong", "_modOldDM":"oldDM", "_newDM":"newDM"}
-    directory22options = {"_1ppi0":"1prongpizero", "_3pold":"3prong_old"}
+    directory2options = {"_1p": "1prong", "_2p": "2prong", "_3p": "3prong", "_modOldDM": "oldDM", "_newDM": "newDM"}
+    directory22options = {"_1ppi0": "1prongpizero", "_3pold": "3prong_old"}
 
 
     for alpha in directory1options:
-	if alpha in header: directory1 = alpha
-    for alpha,beta in directory2options.items():
-	if alpha in header: directory2 = beta
-    for alpha,beta in directory22options.items():
-	if alpha in header: directory2 = beta
+        if alpha in header:
+            directory1 = alpha
+            break
 
-    if "_eta" in header:
-	if directory1: directory1 += "_eta"
-	if directory2: directory2 += "_eta"
+    for key, value in directory2options.items() + directory22options.items():
+        if key in header:
+            directory2 = value
+            break
 
-    if directory1: save(canvas, 'compare_' + runtype + '/' + directory1 + '/' + header)
-    if directory2: save(canvas, 'compare_' + runtype + '/' + directory2 + '/' + header)
-    if not directory1 and not directory2:  save(canvas, 'compare_' + runtype + '/' + header)
-    if "_eta" in header:
-        save(canvas, 'compare_' + runtype + '/all_eta/' + header)
-    else:
-        save(canvas, 'compare_' + runtype + '/all/' + header)
+    if dryrun: return
+ 
+    if directory1:
+        directory1 += _eta
+        save(canvas, 'compare_' + runtype + '/' + directory1 + '/' + header)
 
+    if directory2:
+        directory2 += _eta
+        save(canvas, 'compare_' + runtype + '/' + directory2 + '/' + header)
 
-def hoverlay(hists, xtitle, ytitle, name):
+    if not (directory1 or directory2):
+        save(canvas, 'compare_' + runtype + '/' + header)
+
+    save(canvas, 'compare_' + runtype + '/all' + _eta + '/' + header)
+
+def hoverlay(hists, xtitle, ytitle, name, runtype, tlabel, xlabel, xlabel_eta, dryrun=False):
     c = TCanvas()
     # Upper plot will be in pad1
     pad1 = TPad("pad1", "pad1", 0, 0.3, 1, 1.0)
@@ -157,8 +173,7 @@ def hoverlay(hists, xtitle, ytitle, name):
 
     ymax = 0
     for hist in hists:
-        if ymax < hist.GetMaximum():
-            ymax = hist.GetMaximum()
+        if ymax < hist.GetMaximum(): ymax = hist.GetMaximum()
 
     leg = TLegend(0.2, 0.65, 0.91, 0.9)
     configureLegend(leg, 1)
@@ -168,14 +183,12 @@ def hoverlay(hists, xtitle, ytitle, name):
         ihist.SetMaximum(ymax*1.2)
         ihist.SetMinimum(0.)
         # if c.GetLogy > 0:
-        if pad1.GetLogy > 0:
-            ihist.SetMinimum(0.001)
+        if pad1.GetLogy > 0: ihist.SetMinimum(0.001)
         ihist.SetMarkerSize(0.)
         ihist.GetXaxis().SetTitle(xtitle)
         ihist.GetYaxis().SetTitle(ytitle)
 
-        if ii == 0:
-            ihist.Draw('h')
+        if ii == 0: ihist.Draw('h')
         else:
             ihist.Draw('hsame')
             ihr = ihist.Clone()
@@ -202,7 +215,6 @@ def hoverlay(hists, xtitle, ytitle, name):
 
     tex2.SetTextAlign(10)
     tex2.SetTextFont(42)
-    # tex2.SetTextSize(0.03)
     tex2.SetTextSize(0.043)
     tex2.Draw()
 
@@ -228,7 +240,7 @@ def hoverlay(hists, xtitle, ytitle, name):
             ihist.Draw('epsame')
 
     c.cd()          # Go back to the main canvas
-
+    if dryrun: return
     save(c, 'compare_' + runtype + '/histograms/hist_' + name)
 
 
@@ -270,25 +282,21 @@ def makeEffPlotsVars(tree, varx, vary, sel, nbinx, xmin, xmax, nbiny, ymin, ymax
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    parser.add_argument('runtype', choices=['ZTT', 'ZEE', 'ZMM', 'QCD', 'TTbar', 'TTbarTau', 'ZpTT'], help='choose sample type')
-    parser.add_argument('-r', '--releases', help='comma separated list of releases', default='CMSSW_9_4_0_pre1,CMSSW_9_4_0_pre2')
-    parser.add_argument('-g', '--globaltag', help='comma separated global tags of releases', default='93X_mc2017_realistic_v3-v1,94X_mc2017_realistic_v1-v1')
-    parser.add_argument('-p', '--part',  help='Make WP plots(1), first half of histogram plots(2), second half of histogram plots(3), or everything at once(0) (This part needs to be split up to avoid a crash that happens for some reason)', default=0)
-    parser.add_argument('-b', '--onebin', action="store_true",  help='Plot inclusive efficiencies by only using one bin', default=False)
-    
+    addArguments(parser, compare=True)
     args = parser.parse_args()
-    
-    part = int(args.part)
+    part = args.part
+    dryRun = args.dryRun
+    ptPlotsBinning = array('d', [20, 200]) if args.onebin else array('d', [20, 30, 40, 50, 60, 70, 80, 100, 150, 200])
+    etaPlotsBinning = array('d', [-2.4, 2]) if args.onebin else array('d', [ round(-2.4 + i*0.4,1) for i in range(0, 12 + 1)])
+
     runtype = args.runtype
     print 'Producing plots for runtype', runtype
-    
+
     releases = args.releases.split(',')
-    globaltags = args.globaltag.split(',')
-    
-    print 'Releases', releases
-    print 'Global tags', globaltags
-    
+    print args.globalTag
+
+    globaltags = args.globalTag.split(',')
+    print 'Releases', releases, '\nGlobal tags', globaltags
 
     tlabel = 'Z #rightarrow #tau#tau'
     # tlabel = 'gg #rightarrow H(125) #rightarrow #tau#tau'
@@ -331,21 +339,23 @@ if __name__ == '__main__':
         {'col': 6, 'marker': 22, 'width': 2},
     ]
     
+    print "Collecting samples"
     sampledict = {}
     for i_sample, release in enumerate(globaltags):
         sampledict[release] = styles[i_sample]
         tfile = sampledict[release]['file'] = TFile('Myroot_{}_{}_{}.root'.format(releases[i_sample], release, runtype))
         sampledict[release]['tree'] = tfile.Get('per_tau')
+    print "sampledict:"
+    pp.pprint(sampledict)
 
-    if part<2:
+    print "First part of plots"
+    if part < 2:
         for hname, hdict in vardict.items():
-
             hists = []
             histseta = []
 
             for rel, rdict in sampledict.items():
                 tree = rdict['tree']
-
                 num_sel = reco_cut
                 den_sel = '1'
 
@@ -372,15 +382,10 @@ if __name__ == '__main__':
             continue
 
         hists = []
-
-        if runtype != 'ZTT' and runtype != 'TTbarTau' and hname.find('pt_resolution') != -1:
-            continue
-
         for rel, rdict in sampledict.items():
             tree = rdict['tree']
+            hist = TH1F('h_' + hname + '_' + rel, 'h_' + hname + '_' + rel, hdict['nbin'], hdict['min'], hdict['max'])
 
-            hist = TH1F('h_' + hname + '_' + rel, 'h_' + hname +
-                        '_' + rel, hdict['nbin'], hdict['min'], hdict['max'])
             hist.GetYaxis().SetNdivisions(507)
             hist.SetLineColor(rdict['col'])
             hist.SetLineWidth(rdict['width'])
@@ -390,9 +395,10 @@ if __name__ == '__main__':
             hist.GetXaxis().SetTitle(hdict['title'])
 
             tree.Project(hist.GetName(), hdict['var'], hdict['sel'])
-            if hist.Integral(0, hist.GetNbinsX()+1) > 0:
-                hist.Scale(1./hist.Integral(0, hist.GetNbinsX()+1))
+
+            if hist.Integral(0, hist.GetNbinsX() + 1) > 0:
+                hist.Scale(1. / hist.Integral(0, hist.GetNbinsX() + 1))
 
             hists.append(hist)
 
-        hoverlay(hists, hdict['title'], 'a.u.', hname)
+        hoverlay(hists, hdict['title'], 'a.u.', hname, runtype, options_dict[runtype].tlabel, options_dict[runtype].xlabel, options_dict[runtype].xlabel_eta, dryrun=dryRun)
