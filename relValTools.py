31bac39c75ed011f964ccc698290b884b3422020
Olena Hlushchenko <greyxray@gmail.com>
2018-04-25 14:08:00 +0200

    update relValTools for different scripts usage

--- relValTools.py
+++ relValTools.py
@@ -1,10 +1,11 @@
 import eostools
 import re
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
-def addArguments(parser):
+def addArguments(parser, compare=False):
     # for produceTauValTree.py
     parser.add_argument('--runtype', choices=['ZTT', 'ZEE', 'ZMM', 'QCD', 'TTbar', 'TTbarTau', 'ZpTT'], help='choose sample type')
-    parser.add_argument('-r', '--release', default='CMSSW_9_4_0_pre2',  help='Release string [Default: %(default)s]')
     parser.add_argument('-g', '--globalTag', default='PU25ns_94X_mc2017_realistic_v1-v1',  help='Global tag [Default: %(default)s]')
     parser.add_argument('-n', '--maxEvents', default=-1, type=int,  help='Number of events that will be analyzed (-1 = all events) [Default: %(default)s]')
     parser.add_argument('-u', '--useRecoJets', default=False, action="store_true",  help='Use RecoJets [Default: %(default)s]')
@@ -15,12 +16,16 @@ def addArguments(parser):
     parser.add_argument('-m', '--mvaid', default=["2017v1", "2016v1", "newDM2016v1"], nargs='*',
         help="Select final state(s) for measurement. This agument can be set multiple times. Possible options: [2017v1, 2017v2, newDM2017v2, dR0p32017v2, 2016v1, newDM2016v1]. [Default: %(default)s]")
     parser.add_argument('-t', '--tauCollection', default='slimmedTaus', help="Tau collection to be used. Possible: NewTauIDsEmbedded; [Default: %(default)s].")
-    # for compare.py
-    parser.add_argument('-p', '--part',  help='Make WP plots(1), first half of histogram plots(2), \
-        second half of histogram plots(3), or everything at once(0) \
-        (This part needs to be split up to avoid a crash that happens for some reason)', default=0)
-    parser.add_argument('-b', '--onebin', action="store_true",  help='Plot inclusive efficiencies by only using one bin', default=False)
+    parser.add_argument('--dryRun', default=False, action="store_true",  help='Dry run - no plots [Default: %(default)s]')
 
+    if compare:
+        parser.add_argument('-r', '--releases', default='CMSSW_9_4_0_pre1,CMSSW_9_4_0_pre2', help='comma separated list of releases')
+        parser.add_argument('-p', '--part', default=0, type=int,  help='Make WP plots(1), first half of histogram plots(2), \
+            second half of histogram plots(3), or everything at once(0) \
+            (This part needs to be split up to avoid a crash that happens for some reason)')
+        parser.add_argument('-b', '--onebin', default=False, action="store_true",  help='Plot inclusive efficiencies by only using one bin')
+    else:
+        parser.add_argument('-r', '--release', default='CMSSW_9_4_0_pre2',  help='Release string [Default: %(default)s]')
 
 def getFilesFromEOS(path, cmseospath=True):
     '''Give path in form /store/relval/CMSSW_9_4_0_pre2/...'''
